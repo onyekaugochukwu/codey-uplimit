5ea2cfac4e444676e7d32bcadfeead086e82001e
Ugochukwu Onyeka <92909501+onyekaugochukwu@users.noreply.github.com>
2024-12-12 23:34:41 +0100

    Updates to config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
 # Google Gemini API Key
-GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]  # Replace with your actual key
-
-# # LanceDB settings (optional)
-# LANCEDB_URI = "./lancedb"  # URI for LanceDB
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]  
 
 # Database configuration
 LANCEDB_URI = os.path.join(os.getcwd(), "lancedb")
